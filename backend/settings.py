59503b440a6753e5671a7e2b720c96ae484bde51
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-05-23 21:49:17 +0600

    added routes -products list & single product

--- backend/settings.py
+++ backend/settings.py
@@ -38,12 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+    'rest_framework',
     'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .products import products  
 
 # Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    return Response('Hello')
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for item in products:
+        if item['_id'] == pk:
+            product = item
+            break
+    return Response(product)
+    
