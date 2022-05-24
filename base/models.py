d9a62e97a892d2d946253c0de96e3e8c00fd1f9b
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-05-24 09:54:42 +0600

    Models - added OrderItem table

--- base/models.py
+++ base/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     user            = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     _id             = models.AutoField(primary_key=True, editable=False)
     name            = models.CharField(max_length=200, null=True, blank=True)
-    #image          = models
+    #image           = models.ImageField(null=True, blank=True, default='/placeholder.png')
     description     = models.TextField(null=True, blank=True)
     brand           = models.CharField(max_length=200, null=True, blank=True)
     category        = models.CharField(max_length=200, null=True, blank=True)
@@ -51,3 +51,17 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.createdAt)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    #image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
