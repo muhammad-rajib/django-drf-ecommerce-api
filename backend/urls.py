b5e3944b5b52a2a09f048f34993b754f71c32218
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-05-31 16:01:39 +0600

    added routes for users and products

--- backend/urls.py
+++ backend/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('base.urls')),
+    path('api/products/', include('base.urls.product_urls')),
+    path('api/users/', include('base.urls.user_urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
