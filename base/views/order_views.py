c550356d76398b5cc9d15edfa4d37b5859bd14e9
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-06-04 16:14:45 +0600

    added order service

---
+++ base/views/order_views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
+
+from datetime import datetime
+
+from base.models import Product, Order, OrderItem, ShippingAddress
+from base.serializers import ProductSerializer, OrderSerializer
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'details': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+
+        # Create Order
+        order = Order.objects.create(
+            user            = user,
+            paymentMethod   = data['paymentMethod'],
+            taxPrice        = data['taxPrice'],
+            shippingPrice   = data['shippingPrice'],
+            totalPrice      = data['totalPrice']
+        )
+
+        # Create shipping address
+        shipping = ShippingAddress.objects.create(
+            order       = order,
+            addresss    = data['shippingAddress']['address'],
+            city        = data['shippingAddress']['city'],
+            postalCode  = data['shippingAddress']['postalCode'],
+            country     = data['shippingAddress']['country']
+        )
+
+        # Create Order Items
+        for itm in orderItems:
+            product = Product.objects.get(_id=i['product'])
+
+            item = OrderItem.objects.create(
+                product = product,
+                order   = order,
+                name    = product.name,
+                qty     = itm['qty'],
+                price   = itm['price'],
+                image   = product.img.url,
+            )
+
+            # update stock
+            product.countInStock -= item.qty
+            product.save()
+        
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data)
--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/products/', include('base.urls.product_urls')),
     path('api/users/', include('base.urls.user_urls')),
+    path('api/orders/', include('base.urls.order_urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
