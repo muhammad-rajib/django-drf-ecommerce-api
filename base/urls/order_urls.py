38f151f66a953ca21e49741825acbfa5a3739393
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-06-06 16:10:14 +0600

    order-views - added method to see all ordered list of user

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -4,6 +4,7 @@ from base.views import order_views as views
 
 urlpatterns = [
     path('add/', views.addOrderItems, name='order-add'),
+    path('myorders/', views.getMyOrders, name='my-orders'),
     path('<str:pk>/', views.getOrderById, name='get-order'),
     path('<str:pk>/pay/', views.updateOrderPay, name='order-pay'),
 ]
--- base/views/order_views.py
+++ base/views/order_views.py
@@ -79,6 +79,15 @@ def getOrderById(request, pk):
         return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getMyOrders(request):
+    user = request.user
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def updateOrderPay(request, pk):
