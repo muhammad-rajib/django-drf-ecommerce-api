f5bc575071b975e1032a1b40c3028cb225da0f5d
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-06-07 20:10:51 +0600

    added route and views to delete product by admin

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from base.models import Product
 from base.serializers import ProductSerializer
@@ -20,3 +21,10 @@ def getProduct(request, pk):
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
     
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response("Product Deleted")
+
