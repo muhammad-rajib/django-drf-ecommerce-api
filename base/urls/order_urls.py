83760687da3eb4bcf5728fad281cf2da552f0e16
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-06-08 22:46:12 +0600

    admin user - added route of order list - added view of order list

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -3,6 +3,7 @@ from base.views import order_views as views
 
 
 urlpatterns = [
+    path('', views.getOrders, name='orders'),
     path('add/', views.addOrderItems, name='order-add'),
     path('myorders/', views.getMyOrders, name='my-orders'),
     path('<str:pk>/', views.getOrderById, name='get-order'),
--- base/views/order_views.py
+++ base/views/order_views.py
@@ -99,3 +99,13 @@ def updateOrderPay(request, pk):
     order.save()
 
     return Response("Order was Paid")
+
+
+# Admin Views
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
