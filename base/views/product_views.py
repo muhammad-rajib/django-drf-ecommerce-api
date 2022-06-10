be814fdb4f54c09c3dd9838dffc804b6a6065054
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-06-10 12:39:57 +0600

    added views to return top 5 products list

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -21,7 +21,7 @@ def getProducts(request):
     products = Product.objects.filter(name__icontains=query).order_by('-createdAt')
 
     page = request.query_params.get('page')
-    paginator = Paginator(products, 2)
+    paginator = Paginator(products, 5)
 
     try:
         products = paginator.page(page)
@@ -36,6 +36,14 @@ def getProducts(request):
     page = int(page)
     serializer = ProductSerializer(products, many=True)
     return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages})
+
+
+@api_view(['GET'])
+def getTopProducts(request):
+    # top 5 products
+    products = Product.objects.filter(rating__gte=4).order_by("-rating")[0:5]
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
