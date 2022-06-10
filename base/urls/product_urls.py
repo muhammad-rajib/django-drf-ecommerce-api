77f912e0fdcec62bca98105c8b7b0e335a2e57d4
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-06-10 12:39:49 +0600

    added route for top 5 products

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -4,11 +4,12 @@ from base.views import product_views as views
 
 urlpatterns = [
     path('', views.getProducts, name='products'),
+    path('top/', views.getTopProducts, name='top-products'),
+    path('<str:pk>/', views.getProduct, name="product"),
 
     path('<str:pk>/reviews/', views.createProductReview, name="product-reviews"),
     
     path('create/', views.createProduct, name="create-product"),
-    path('<str:pk>/', views.getProduct, name="product"),
     path('update/<str:pk>/', views.updateProduct, name="update-product"),
     path('upload/image/', views.uploadImage, name='upload-image'),
     path('delete/<str:pk>', views.deleteProduct, name="delete-product"),
